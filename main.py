8d24a87c07d1c80b6cc9dabd4814cf7106e68633
Francisco Villacis <fm1024790@gmail.com>
2026-08-27 20:26:11 -0500

    Change Telegram token in main.py
    Updated the Telegram token in the configuration.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import threading
 import os
 
 # Configuración del Token
-TOKEN = os.environ.get('TELEGRAM_TOKEN', '8514532683:AAFzZbsWs8-dDIMl01ROjjuo5TnyUL6dgL0')
+TOKEN = os.environ.get('TELEGRAM_TOKEN', '8514532683:AAGBc0230C-VoXM-iPN5QbmGQcIlOkfP864')
 bot = telebot.TeleBot(TOKEN)
 cache_enlaces = {}
 
